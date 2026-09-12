c22afef45d5a46a3e8c7ef2354294a8e1cb5bc93
william sumba <williamsumba2019@gmailcom>
2026-09-12 14:28:19 +0300

    modified blocking_resources

--- blocking_resources.js
+++ blocking_resources.js
@@ -1,12 +1,38 @@
 'use strict'
 
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer"
 
-(async()=>{
+(async () =>{
+        const browser = await puppeteer.launch({headless:false})
+        const page = await browser.newPage()
 
-    const browser = await puppeteer.launch({headless:false})
-    const page = await browser.newPage()
+        const url = "https://www.jumia.co.ke/?srsltid=AfmBOorA6D94ySRsYgALVKlP-bmALGzGoXKpeCL3pT-SysH_kthojf0W";
 
-   
+        
+        try{
+            //enable request interception
+            await page.setRequestInterception(true);
+
+            page.on('request', (req) => {
+            const resourceType = req.resourceType();
+            const blockedTypes = ['image', 'stylesheet', 'font', 'media'];
+
+            if (blockedTypes.includes(resourceType)) {
+            req.abort();
+            } else {
+            req.continue();
+            }
+             });
+
+            await page.goto(url, {waitUntil: "domcontentloaded", timeout:6000})
+
+
+
+        }catch(err){
+           console.error(err.message)
+        }
+        finally{
+           await browser.close()
+        }
 
 })()
